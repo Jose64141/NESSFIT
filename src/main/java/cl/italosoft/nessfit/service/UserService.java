5d30d5045f973adb64d9f2c32985b8fd67200810
Jose Alcayaga <jose64am@aol.com>
2022-09-22 20:12:05 -0300

    feat: UserService Interface methods added

--- src/main/java/cl/italosoft/nessfit/service/UserService.java
+++ src/main/java/cl/italosoft/nessfit/service/UserService.java
@@ -1,4 +1,15 @@
 package cl.italosoft.nessfit.service;
 
-public interface UserService {
+
+import cl.italosoft.nessfit.model.User;
+
+import java.util.List;
+
+public interface UserService
+{
+    public void save(User user);
+
+    public void delete(String userRut);
+
+    public List<User> list();
 }
