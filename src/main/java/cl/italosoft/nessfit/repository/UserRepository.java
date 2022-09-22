b3a4b4fffd85e0e0449988fbfd7d93df38337a1a
Jose Alcayaga <jose64am@aol.com>
2022-09-22 19:38:37 -0300

    docs: UserRepository class documented

--- src/main/java/cl/italosoft/nessfit/repository/UserRepository.java
+++ src/main/java/cl/italosoft/nessfit/repository/UserRepository.java
@@ -3,6 +3,9 @@ package cl.italosoft.nessfit.repository;
 import cl.italosoft.nessfit.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository for User Entity
+ */
 public interface UserRepository extends JpaRepository<User, String>
 {
 }
