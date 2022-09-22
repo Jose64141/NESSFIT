0f3f30398c952fa809835a007f10b34f0f9856a3
David Araya <david.araya@alumnos.ucn.cl>
2022-09-22 16:16:36 -0300

    style: space in main

--- src/main/java/cl/italosoft/nessfit/NessfitApplication.java
+++ src/main/java/cl/italosoft/nessfit/NessfitApplication.java
@@ -1,5 +1,6 @@
 package cl.italosoft.nessfit;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
