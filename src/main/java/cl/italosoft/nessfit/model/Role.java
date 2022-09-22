445b62dd57db6884e1e86aaaba2c5d028f689151
David Araya <david.araya@alumnos.ucn.cl>
2022-09-22 16:36:29 -0300

    feat: Role Class serializabled

--- src/main/java/cl/italosoft/nessfit/model/Role.java
+++ src/main/java/cl/italosoft/nessfit/model/Role.java
@@ -3,13 +3,14 @@ package cl.italosoft.nessfit.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 /**
  * Model for Role Entity
  */
 @Entity
 @Table(name = "roles")
-public class Role 
+public class Role implements Serializable
 {
     @Id
     private int id;
