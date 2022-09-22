1f07eddff308fd12093d37a9e34246c876a5c36e
David Araya <david.araya@alumnos.ucn.cl>
2022-09-22 16:55:26 -0300

    docs: User Class all methods documentated

--- src/main/java/cl/italosoft/nessfit/model/User.java
+++ src/main/java/cl/italosoft/nessfit/model/User.java
@@ -26,81 +26,145 @@ public class User implements Serializable
     @JoinColumn(name = "role_id",referencedColumnName = "id")
     private Role role;
 
+    /**
+     * Gets the user rut
+     * @return Current user rut
+     */
     public String getRut()
     {
         return rut;
     }
 
+    /**
+     * Sets the user rut
+     * @param rut New rut for user
+     */
     public void setRut(String rut)
     {
         this.rut = rut;
     }
 
+    /**
+     * Gets the username
+     * @return Current username
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * Sets the username
+     * @param name New name for user
+     */
     public void setName(String name)
     {
         this.name = name;
     }
 
+    /**
+     * Gets the user lastname
+     * @return Current user lastname
+     */
     public String getLastName()
     {
         return lastName;
     }
 
+    /**
+     * Sets the user lastname
+     * @param lastName New lastname for user
+     */
     public void setLastName(String lastName)
     {
         this.lastName = lastName;
     }
 
+    /**
+     * Gets the user password
+     * @return Current user password
+     */
     public String getPassword()
     {
         return password;
     }
 
+    /**
+     * Sets the user password
+     * @param password New password for user
+     */
     public void setPassword(String password)
     {
         this.password = password;
     }
 
+    /**
+     * Gets the user phone number
+     * @return Current user phone number
+     */
     public int getPhoneNumber()
     {
         return phoneNumber;
     }
 
+    /**
+     * Sets the user phone number
+     * @param phoneNumber New user phone number
+     */
     public void setPhoneNumber(int phoneNumber)
     {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * Gets the user e-mail
+     * @return Current user e-mail
+     */
     public String getEmail()
     {
         return email;
     }
 
+    /**
+     * Sets the user e-mail
+     * @param email New user e-mail
+     */
     public void setEmail(String email)
     {
         this.email = email;
     }
 
+    /**
+     * Gets the user status
+     * @return Current user status
+     */
     public boolean isStatus()
     {
         return status;
     }
 
+    /**
+     * Set the user status
+     * @param status New user status
+     */
     public void setStatus(boolean status)
     {
         this.status = status;
     }
 
+    /**
+     * Gets the user role
+     * @return user role
+     */
     public Role getRole()
     {
         return role;
     }
 
+    /**
+     * Sets the user role
+     * @param role New user role
+     */
     public void setRole(Role role)
     {
         this.role = role;
