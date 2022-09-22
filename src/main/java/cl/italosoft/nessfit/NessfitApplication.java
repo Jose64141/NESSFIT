d172de18e6f1168c9e154f19a225dbfcbe56bb3c
David Araya <david.araya@alumnos.ucn.cl>
2022-09-22 14:47:27 -0300

    style: brackets style format updated

--- src/main/java/cl/italosoft/nessfit/NessfitApplication.java
+++ src/main/java/cl/italosoft/nessfit/NessfitApplication.java
@@ -2,12 +2,13 @@ package cl.italosoft.nessfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class NessfitApplication {
-
-    public static void main(String[] args) {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class NessfitApplication
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(NessfitApplication.class, args);
     }
-
 }
