a89049b9a7eff5c649c8579de31fe7fdc0163539
Jose Alcayaga <jose64am@aol.com>
2022-09-22 12:45:14 -0300

    Initial commit

---
+++ src/main/java/cl/italosoft/nessfit/NessfitApplication.java
@@ -0,0 +1,13 @@
+package cl.italosoft.nessfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NessfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NessfitApplication.class, args);
+    }
+
+}
