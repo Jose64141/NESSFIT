e6af01a10cd81222dde8b0e6876daaaca1babd5e
Jose Alcayaga <jose64am@aol.com>
2022-09-22 20:24:34 -0300

    docs: UserService Interface methods documented

--- src/main/java/cl/italosoft/nessfit/service/UserService.java
+++ src/main/java/cl/italosoft/nessfit/service/UserService.java
@@ -5,11 +5,27 @@ import cl.italosoft.nessfit.model.User;
 
 import java.util.List;
 
+/**
+ * Interface for the User Service
+ */
 public interface UserService
 {
+
+    /**
+     * Saves a given user record.
+     * @param user the user to save.
+     */
     public void save(User user);
 
+    /**
+     * Deletes a user record.
+     * @param userRut the id of the record to delete.
+     */
     public void delete(String userRut);
 
+    /**
+     * Returns all user records.
+     * @return a list containing the records.
+     */
     public List<User> list();
 }
