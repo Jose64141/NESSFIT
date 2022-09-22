470cef0817f72dd1e680807fcfc97a073ed44505
Jose Alcayaga <jose64am@aol.com>
2022-09-22 20:29:36 -0300

    feat: UserServiceImpl Class created and added methods with documentation

---
+++ src/main/java/cl/italosoft/nessfit/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package cl.italosoft.nessfit.service;
+
+import cl.italosoft.nessfit.model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService
+{
+    /**
+     * Saves a given user record.
+     * @param user the user to save.
+     */
+    @Override
+    public void save(User user)
+    {
+
+    }
+
+    /**
+     * Deletes a user record.
+     * @param userRut the id of the record to delete.
+     */
+    @Override
+    public void delete(String userRut)
+    {
+
+    }
+
+    /**
+     * Returns all user records.
+     * @return a list containing the records.
+     */
+    @Override
+    public List<User> list()
+    {
+        return null;
+    }
+}
