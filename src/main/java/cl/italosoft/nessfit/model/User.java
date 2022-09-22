cd6a3604b533fcf0a9293d1f13502a2e514c1a65
David Araya <david.araya@alumnos.ucn.cl>
2022-09-22 16:39:07 -0300

    feat: Getters and Setters for User Class

--- src/main/java/cl/italosoft/nessfit/model/User.java
+++ src/main/java/cl/italosoft/nessfit/model/User.java
@@ -25,4 +25,84 @@ public class User implements Serializable
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "role_id",referencedColumnName = "id")
     private Role role;
+
+    public String getRut()
+    {
+        return rut;
+    }
+
+    public void setRut(String rut)
+    {
+        this.rut = rut;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public int getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public boolean isStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(boolean status)
+    {
+        this.status = status;
+    }
+
+    public Role getRole()
+    {
+        return role;
+    }
+
+    public void setRole(Role role)
+    {
+        this.role = role;
+    }
 }
