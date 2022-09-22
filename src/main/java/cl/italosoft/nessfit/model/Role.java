36ec1312fdadf6ed9de0ef8bced31e64c2c1c24c
David Araya <david.araya@alumnos.ucn.cl>
2022-09-22 16:32:38 -0300

    feat: Role Class mapped for DB

--- src/main/java/cl/italosoft/nessfit/model/Role.java
+++ src/main/java/cl/italosoft/nessfit/model/Role.java
@@ -1,10 +1,17 @@
 package cl.italosoft.nessfit.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Model for Role Entity
  */
+@Entity
+@Table(name = "roles")
 public class Role
 {
+    @Id
     private int id;
     private String name;
 }
