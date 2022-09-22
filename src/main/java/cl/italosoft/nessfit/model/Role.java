b88c7ed684b89c2eeabaad481f103f9028412242
David Araya <david.araya@alumnos.ucn.cl>
2022-09-22 16:59:52 -0300

    docs: Role Class all methods documentated

--- src/main/java/cl/italosoft/nessfit/model/Role.java
+++ src/main/java/cl/italosoft/nessfit/model/Role.java
@@ -16,22 +16,37 @@ public class Role implements Serializable
     private int id;
     private String name;
 
-    
+    /**
+     * Gets the role id
+     * @return Current role id
+     */
     public int getId()
     {
         return id;
     }
 
+    /**
+     * Sets the role id
+     * @param id New role id
+     */
     public void setId(int id)
     {
         this.id = id;
     }
 
+    /**
+     * Gets the role name
+     * @return Current role name
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * Sets the role name
+     * @param name New role name
+     */
     public void setName(String name)
     {
         this.name = name;
