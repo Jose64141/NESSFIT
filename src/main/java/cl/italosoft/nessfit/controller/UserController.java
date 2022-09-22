9a6beda4fd1037b5e7bf3db9837159b06823b06d
David Araya <david.araya@alumnos.ucn.cl>
2022-09-22 19:10:24 -0300

    feat: Controller package and UserController Class Added

--- src/main/java/cl/italosoft/nessfit/controller/UserController.java
+++ src/main/java/cl/italosoft/nessfit/controller/UserController.java
@@ -1,5 +1,22 @@
 package cl.italosoft.nessfit.controller;
 
+
+import cl.italosoft.nessfit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController
 {
+    /*
+    @Autowired
+    private UserService userService;
+    @GetMapping("/name")
+    public String save()
+    {
+
+        return "save";
+    }
+     */
 }
