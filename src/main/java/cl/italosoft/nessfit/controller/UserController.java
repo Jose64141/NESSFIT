a03f3b5e111148971c56c3a83a8ee2c18e95bd4c
David Araya <david.araya@alumnos.ucn.cl>
2022-09-23 00:13:59 -0300

    style: CSS added to login page

--- src/main/java/cl/italosoft/nessfit/controller/UserController.java
+++ src/main/java/cl/italosoft/nessfit/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController
 {
-
     @Autowired
     private UserService userService;
     @GetMapping("/saveTestUser")
