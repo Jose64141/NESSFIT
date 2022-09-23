aca3249af0f9215df26980330a3a1efb12feb540
David Araya <david.araya@alumnos.ucn.cl>
2022-09-23 02:19:26 -0300

    feat: StyleColorPalette.txt added, contains color and code for the page style

--- src/main/java/cl/italosoft/nessfit/controller/LoginController.java
+++ src/main/java/cl/italosoft/nessfit/controller/LoginController.java
@@ -3,6 +3,7 @@ package cl.italosoft.nessfit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController
@@ -12,4 +13,6 @@ public class LoginController
     {
         return "login";
     }
+
+
 }
