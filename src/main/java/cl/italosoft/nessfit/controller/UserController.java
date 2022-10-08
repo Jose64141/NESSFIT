2d67fe04124d33d0d7d59cf16275f6ed965478db
Jose Alcayaga <jose64am@aol.com>
2022-10-08 03:23:26 -0300

    feat: implemented user info change feature

--- src/main/java/cl/italosoft/nessfit/controller/UserController.java
+++ src/main/java/cl/italosoft/nessfit/controller/UserController.java
@@ -3,6 +3,7 @@ package cl.italosoft.nessfit.controller;
 
 import cl.italosoft.nessfit.model.User;
 import cl.italosoft.nessfit.service.UserService;
+import cl.italosoft.nessfit.util.RutValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,14 +12,15 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @Controller
 public class UserController
@@ -26,7 +28,13 @@ public class UserController
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RutValidator rutValidator;
 
+    @InitBinder("user")
+    public void initBinder(WebDataBinder binder) {
+        binder.addValidators(rutValidator);
+    }
     @GetMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response)
     {
@@ -50,7 +58,7 @@ public class UserController
     }
 
     @PostMapping("/account-settings/{operation}")
-    public String configChange(HttpServletRequest request, HttpServletResponse response, Model model, @PathVariable String operation, @RequestBody MultiValueMap<String, String> formBody, RedirectAttributes attr) //https://stackoverflow.com/a/55338584
+    public String configChange(HttpServletRequest request, HttpServletResponse response, Model model, @PathVariable String operation, @RequestBody MultiValueMap<String, String> formBody, RedirectAttributes attr, @Valid User newInfo, BindingResult result) //https://stackoverflow.com/a/55338584
     {
         // do this, do that or just give an error
         User user = this.userService.find(request.getRemoteUser());
@@ -97,7 +105,47 @@ public class UserController
                 return "redirect:/login";
             case "change-data":
                 attr.addFlashAttribute("showPassword", false);
-                // validate email
+                if(result.hasErrors())
+                {
+                    StringBuffer errorMsg = new StringBuffer();
+                    Boolean[] msgInserted = {false, false, false};
+                    for (FieldError error: result.getFieldErrors())
+                    {
+                        switch (error.getField())
+                        {
+                            case "name", "firstLastName", "secondLastName":
+                                if(!msgInserted[0])
+                                {
+                                    errorMsg.append("Los nombres o apellidos deben tener más de 2 caracteres. ");
+                                    msgInserted[0] = true;
+                                }
+                                break;
+                            case "phoneNumber":
+                                if(!msgInserted[1])
+                                {
+                                    errorMsg.append("El teléfono móvil ingresado no es válido. ");
+                                    msgInserted[1] = true;
+                                }
+                                break;
+                            case "email":
+                                if(!msgInserted[2])
+                                {
+                                    errorMsg.append("Su correo electrónico no es válido. ");
+                                    msgInserted[2] = true;
+                                }
+                                break;
+                        }
+                    }
+                    attr.addFlashAttribute("infoErrorMsg",errorMsg);
+                    break;
+                }
+                user.setName(newInfo.getName().strip());
+                user.setFirstLastName(newInfo.getFirstLastName().strip());
+                user.setSecondLastName(newInfo.getSecondLastName().strip());
+                user.setPhoneNumber(newInfo.getPhoneNumber());
+                user.setEmail(newInfo.getEmail().strip());
+                userService.saveAndFlush(user);
+                attr.addFlashAttribute("infoSuccessMsg","Los cambios se han realizado con éxito.");
             default:
                 break;
         }
