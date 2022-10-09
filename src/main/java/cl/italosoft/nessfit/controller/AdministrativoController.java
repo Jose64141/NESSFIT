0e2b77d6ef05fef91d18831bd4df901a7435b5a2
David Araya <david.araya@alumnos.ucn.cl>
2022-10-09 03:21:09 -0300

    feat: manage clients button to change isEnabled added

--- src/main/java/cl/italosoft/nessfit/controller/AdministrativoController.java
+++ src/main/java/cl/italosoft/nessfit/controller/AdministrativoController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -28,9 +32,29 @@ public class AdministrativoController
                 model.addAttribute("name",user.getName());
                 model.addAttribute("firstLastName",user.getLastName());
                 model.addAttribute("secondLastName",user.getLastName());
+                model.addAttribute("isEnabled",user.isEnabled() ? "Habilitado" : "Deshabilitado");
+                model.addAttribute("actionName",user.isEnabled() ? "Deshabilitar" : "Habilitar");
+            }
+            else
+            {
+                model.addAttribute("nullMessage","No se han encontrado coincidencias");
             }
         }
+        else
+        {
+            model.addAttribute("nullMessage","No ha seleccionado ningún usuario");
+        }
         return "/administrativo/manage-clients";
+    }
+
+    @PostMapping("/administrativo/manage-clients")
+    public String manageUsers(@RequestParam String rut, RedirectAttributes attr)
+    {
+        User user = this.userService.find(rut);
+        user.setEnabled(!user.isEnabled());
+        this.userService.saveAndFlush(user);
+        attr.addFlashAttribute("isEnabledChanged","Se ha cambiado el estado con éxito");
+        return "redirect:/administrativo/manage-clients?rut="+rut;
     }
 
 
