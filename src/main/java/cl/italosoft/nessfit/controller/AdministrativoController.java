4585a315b2ea0b63dede26a108210532d9d1755d
David Araya <david.araya@alumnos.ucn.cl>
2022-10-09 00:28:45 -0300

    feat: search by rut added

--- src/main/java/cl/italosoft/nessfit/controller/AdministrativoController.java
+++ src/main/java/cl/italosoft/nessfit/controller/AdministrativoController.java
@@ -1,16 +1,37 @@
 package cl.italosoft.nessfit.controller;
 
+import cl.italosoft.nessfit.model.User;
+import cl.italosoft.nessfit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AdministrativoController
 {
+    @Autowired
+    private UserService userService;
+    
     @GetMapping("/administrativo/manage-clients")
-    public String manageUsers(HttpServletRequest request)
+    public String manageUsers(HttpServletRequest request, @RequestParam(required = false) String rut, Model model)
     {
+        if(rut != null)
+        {
+            User user = this.userService.find(rut);
+            if(user != null)
+            {
+                System.out.println(user.getName());
+                model.addAttribute("RUT",rut);
+                model.addAttribute("name",user.getName());
+                model.addAttribute("firstLastName",user.getLastName());
+                model.addAttribute("secondLastName",user.getLastName());
+            }
+        }
         return "/administrativo/manage-clients";
     }
+
+
 }
