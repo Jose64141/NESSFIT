0da3b85deaa4db461e17361012c7bb12e46d9b65
David Araya <david.araya@alumnos.ucn.cl>
2022-10-09 04:05:34 -0300

    feat: Administrador users management page finished

--- src/main/java/cl/italosoft/nessfit/controller/AdministradorController.java
+++ src/main/java/cl/italosoft/nessfit/controller/AdministradorController.java
@@ -7,32 +7,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class AdministrativoController
+public class AdministradorController
 {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/administrativo/manage-clients")
+    @GetMapping("/administrador/manage-clients")
     public String manageUsers(HttpServletRequest request, @RequestParam(required = false) String rut, Model model)
     {
         if(rut != null)
         {
-            User user = this.userService.find(rut,3);
+            User user = this.userService.find(rut);
             if(user != null)
             {
-                model.addAttribute("RUT",rut);
-                model.addAttribute("name",user.getName());
-                model.addAttribute("firstLastName",user.getLastName());
-                model.addAttribute("secondLastName",user.getLastName());
-                model.addAttribute("isEnabled",user.isEnabled() ? "Habilitado" : "Deshabilitado");
-                model.addAttribute("actionName",user.isEnabled() ? "Deshabilitar" : "Habilitar");
+                if(user.getRole().getId() != 1)
+                {
+                    model.addAttribute("RUT",rut);
+                    model.addAttribute("name",user.getName());
+                    model.addAttribute("firstLastName",user.getLastName());
+                    model.addAttribute("secondLastName",user.getLastName());
+                    model.addAttribute("isEnabled",user.isEnabled() ? "Habilitado" : "Deshabilitado");
+                    model.addAttribute("actionName",user.isEnabled() ? "Deshabilitar" : "Habilitar");
+                }
+                else
+                {
+                    model.addAttribute("nullMessage","No se han encontrado coincidencias");
+                }
             }
             else
             {
@@ -43,18 +49,16 @@ public class AdministrativoController
         {
             model.addAttribute("nullMessage","No ha seleccionado ningún usuario");
         }
-        return "administrativo/manage-clients";
+        return "administrador/manage-clients";
     }
 
-    @PostMapping("/administrativo/manage-clients")
+    @PostMapping("/administrador/manage-clients")
     public String manageUsers(@RequestParam String rut, RedirectAttributes attr)
     {
         User user = this.userService.find(rut);
         user.setEnabled(!user.isEnabled());
         this.userService.saveAndFlush(user);
         attr.addFlashAttribute("isEnabledChanged","Se ha cambiado el estado con éxito");
-        return "redirect:/administrativo/manage-clients?rut="+rut;
+        return "redirect:/administrador/manage-clients?rut="+rut;
     }
-
-
 }
