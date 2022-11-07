993b5f710131a46192a9e8d4d372820588ea1725
David Araya <david.araya@alumnos.ucn.cl>
2022-11-07 17:14:02 -0300

    feat: controller, template and css added

--- src/main/java/cl/italosoft/nessfit/controller/AdministradorController.java
+++ src/main/java/cl/italosoft/nessfit/controller/AdministradorController.java
@@ -1,22 +1,39 @@
 package cl.italosoft.nessfit.controller;
 
+import cl.italosoft.nessfit.model.Role;
 import cl.italosoft.nessfit.model.User;
 import cl.italosoft.nessfit.service.UserService;
+import cl.italosoft.nessfit.util.RutValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 public class AdministradorController
 {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RutValidator rutValidator;
+
+    @InitBinder("user")
+    public void initBinder(WebDataBinder binder)
+    {
+        binder.addValidators(rutValidator);
+    }
 
     @GetMapping("/administrador/manage-clients")
     public String manageUsers(HttpServletRequest request, @RequestParam(required = false) String rut, Model model)
@@ -61,4 +78,79 @@ public class AdministradorController
         attr.addFlashAttribute("isEnabledChanged","Se ha cambiado el estado con éxito");
         return "redirect:/administrador/manage-clients?rut="+rut;
     }
+
+    @GetMapping("/administrador/add-administrative")
+    public String addAdministrative(Model model)
+    {
+        model.addAttribute("user", new User());
+        return "administrador/add-administrative";
+    }
+
+    @PostMapping("/administrador/add-administrative")
+    public String addAdministrative(Model model, @Valid User newUser, BindingResult result, RedirectAttributes attr)
+    {
+        if(result.hasErrors())
+        {
+            StringBuffer errorMsg = new StringBuffer();
+            Boolean[] msgInserted = {false, false, false, false};
+            for (FieldError error: result.getFieldErrors())
+            {
+                switch (error.getField())
+                {
+                    case "name", "firstLastName", "secondLastName":
+                        if(!msgInserted[0])
+                        {
+                            errorMsg.append("Los nombres o apellidos deben tener más de 2 caracteres. ");
+                            msgInserted[0] = true;
+                        }
+                        break;
+                    case "phoneNumber":
+                        if(!msgInserted[1])
+                        {
+                            errorMsg.append("El teléfono móvil ingresado no es válido. ");
+                            msgInserted[1] = true;
+                        }
+                        break;
+                    case "email":
+                        if(!msgInserted[2])
+                        {
+                            errorMsg.append("Su correo electrónico no es válido. ");
+                            msgInserted[2] = true;
+                        }
+                        break;
+                    case "rut":
+                        if(!msgInserted[3])
+                        {
+                            errorMsg.append("RUT inválido. ");
+                            msgInserted[3] = true;
+                        }
+                        break;
+                }
+            }
+            attr.addFlashAttribute("errorMsg",errorMsg);
+            return "redirect:/administrador/add-administrative";
+        }
+
+        newUser.setRut(newUser.getRut().toUpperCase());
+
+        User user = this.userService.findByRutOrEmail(newUser.getRut(), newUser.getEmail());
+        if(user != null)
+        {
+            attr.addFlashAttribute("errorMsg","El RUT y/o correo electrónico ya existen en el sistema. Intente iniciar sesión.");
+            return "redirect:/administrador/add-administrative";
+        }
+
+        newUser.setEnabled(true);
+        Role role = new Role(2);
+        newUser.setRole(role);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String newPasswordHash = passwordEncoder.encode(newUser.getRut());
+        newUser.setPassword(newPasswordHash);
+        userService.saveAndFlush(newUser);
+
+        attr.addFlashAttribute("successMsg","El Administrativo se añadió con éxito. ");
+        return "redirect:/administrador/add-administrative";
+    }
+
+
 }
