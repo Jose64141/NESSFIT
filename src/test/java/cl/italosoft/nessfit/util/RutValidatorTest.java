da45eaa3ce296ee2f2932e1acf3a446e2019e3e4
Jose Alcayaga <jose64am@aol.com>
2022-11-11 03:21:47 -0300

    test: finished RutValidator unit test

--- src/test/java/cl/italosoft/nessfit/util/RutValidatorTest.java
+++ src/test/java/cl/italosoft/nessfit/util/RutValidatorTest.java
@@ -1,7 +1,11 @@
 package cl.italosoft.nessfit.util;
 
+import cl.italosoft.nessfit.model.Role;
+import cl.italosoft.nessfit.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.validation.DirectFieldBindingResult;
+import org.springframework.validation.Errors;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -12,15 +16,41 @@ class RutValidatorTest {
 
     private RutValidator validator = new RutValidator();
     @Test
-    void supports() {
+    void supports()
+    {
+        User user = new User();
+        Role notUser = new Role();
+        assertTrue(validator.supports(user.getClass()));
+        assertFalse(validator.supports(notUser.getClass()));
     }
 
     @Test
-    void validate() {
+    void validate()
+    {
+        User user = new User();
+        Errors errors = new DirectFieldBindingResult(user, "user");
+        validator.validate(user, errors);
+        assertEquals(1,errors.getErrorCount());
+
+        errors = new DirectFieldBindingResult(user, "user");
+        user.setRut("209674327");
+        validator.validate(user, errors);
+        assertEquals(0,errors.getErrorCount());
+
+        errors = new DirectFieldBindingResult(user, "user");
+        user.setRut("20967432k");
+        validator.validate(user, errors);
+        assertEquals(1,errors.getErrorCount());
+
+        errors = new DirectFieldBindingResult(user, "user");
+        user.setRut("20967432-k");
+        validator.validate(user, errors);
+        assertEquals(1,errors.getErrorCount());
     }
 
     @Test
-    void checkFormat() {
+    void checkFormat()
+    {
 
         String rut = "209674327";
         assertTrue(validator.checkFormat(rut));
@@ -35,13 +65,15 @@ class RutValidatorTest {
     }
 
     @Test
-    void getNumbers() {
+    void getNumbers()
+    {
         String rut = "209674327";
         assertEquals("20967432", validator.getNumbers(rut,9));
     }
 
     @Test
-    void getCheckDigit() {
+    void getCheckDigit()
+    {
             String rut = "20967432";
             assertEquals('7', validator.getCheckDigit(rut));
             rut = "17977139";
