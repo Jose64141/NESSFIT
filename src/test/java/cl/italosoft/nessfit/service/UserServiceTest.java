1387e83410cd3a581c9d92d45f7449b343d96a6c
David Araya <david.araya@alumnos.ucn.cl>
2022-11-12 23:16:00 -0300

    feat: test added to user service

---
+++ src/test/java/cl/italosoft/nessfit/service/UserServiceTest.java
@@ -0,0 +1,65 @@
+package cl.italosoft.nessfit.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest
+{
+
+    @Test
+    void find()
+    {
+
+    }
+
+    @Test
+    void testFind()
+    {
+
+    }
+
+    @Test
+    void findByEmail()
+    {
+
+    }
+
+    @Test
+    void findByRutWithRole()
+    {
+
+    }
+
+    @Test
+    void save()
+    {
+
+    }
+
+    @Test
+    void saveAndFlush()
+    {
+
+    }
+
+    @Test
+    void delete()
+    {
+
+    }
+
+    @Test
+    void list()
+    {
+
+    }
+
+    @Test
+    void flush()
+    {
+
+    }
+}
